e47b59a7d2b7bca166ca18db456558a146e86748
Mark Nygaard <mark.nygaard@hotmail.com>
2023-01-15 00:35:36 +0100

    only render image if there is one

--- src/components/Sections/Header/Header.tsx
+++ src/components/Sections/Header/Header.tsx
@@ -11,16 +11,18 @@ export default function Header({ details }: any) {
         'flex flex-col items-center bg-[#1e262b] px-2 pt-36 md:px-10'
       )}
     >
-      <div className='relative h-44 w-5/6 md:w-2/3'>
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        <Image
-          data={details.image?.responsiveImage}
-          layout='fill'
-          objectFit='contain'
-          objectPosition='50% 50%'
-          priority
-        />
-      </div>
+      {details.image && (
+        <div className='relative h-44 w-5/6 md:w-2/3'>
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
+          <Image
+            data={details.image?.responsiveImage}
+            layout='fill'
+            objectFit='contain'
+            objectPosition='50% 50%'
+            priority
+          />
+        </div>
+      )}
       <motion.button
         whileHover={{ scale: 1.1 }}
         className='mt-8 bg-[#be965a] py-4 px-7 text-lg font-light text-white'
