aff460bc3a7d5aa15ba5f98b3855f1f901bd0117
Mark Nygaard <mark.nygaard@hotmail.com>
2023-01-15 18:30:03 +0100

    disabled intro animation on slug

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -139,13 +139,13 @@ export default function Page({
             )}
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: '100%' }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.6,
-              ease: [0.43, 0.13, 0.23, 0.96],
-              delay: 1.6,
-            }}
+          // initial={{ opacity: 0, y: '100%' }}
+          // animate={{ opacity: 1, y: 0 }}
+          // transition={{
+          //   duration: 0.6,
+          //   ease: [0.43, 0.13, 0.23, 0.96],
+          //   delay: 1.6,
+          // }}
           >
             <AnimatePresence>
               {page.content?.map((section: any, i: any) => {
