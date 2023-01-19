1f1b6fe698f008366ec6897f0721ec40d336401d
Mark Nygaard <mark.nygaard@hotmail.com>
2023-01-19 20:30:32 +0100

    changed mobile nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({ introPage, page }: any) {
       <div className='hidden w-full lg:sticky lg:top-0 lg:z-40 lg:flex'>
         <Navigation introPage={introPage} page={page} />
       </div>
-      <div className='fixed z-30 flex p-8 text-3xl text-white lg:hidden'>
+      <div className='absolute z-30 flex w-full justify-end p-8 text-3xl text-white lg:hidden'>
         <button onClick={() => setMenuIsOpen(!menuIsOpen)}>
           <span className='sr-only'>Open main menu</span>
           {!menuIsOpen ? <RiMenuFill className='rotate-90' /> : <RiCloseLine />}
