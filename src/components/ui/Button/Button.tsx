2d7ccd1387f168c251e46c69716aadfd327486ad
Mark Nygaard <mark.nygaard@hotmail.com>
2023-06-26 19:42:31 +0200

    Initial storybook setup

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -21,10 +21,12 @@ const buttonStyles = cva(['font-light', 'shadow-lg', 'py-4', 'xl:py-6'], {
     size: {
       small: ['text-sm', 'py-1', 'px-2'],
       medium: ['text-lg', 'py-2', 'px-4'],
+      large: ['text-2xl', 'py-3', 'px-6'],
     },
     width: {
       small: ['px-4', 'xl:px-12'],
       medium: ['px-8', 'xl:px-16'],
+      large: ['px-12', 'xl:px-20'],
     },
   },
   compoundVariants: [{ intent: 'primary', size: 'medium', class: 'uppercase' }],
@@ -37,15 +39,23 @@ const buttonStyles = cva(['font-light', 'shadow-lg', 'py-4', 'xl:py-6'], {
 
 export interface ButtonProps
   extends React.HTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonStyles> {}
+    VariantProps<typeof buttonStyles> {
+  intent?: 'primary' | 'secondary';
+  size?: 'small' | 'medium' | 'large';
+  width?: 'small' | 'medium' | 'large';
+  label: string;
+}
 
 export const Button: React.FC<ButtonProps> = ({
   intent,
   size,
   width,
+  label,
   ...props
 }: ButtonProps) => {
   return (
-    <button className={buttonStyles({ intent, size, width })} {...props} />
+    <button className={buttonStyles({ intent, size, width })} {...props}>
+      {label}
+    </button>
   );
 };
