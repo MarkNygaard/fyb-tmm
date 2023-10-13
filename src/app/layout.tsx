1779bb692d152843fc7283eb44b4933bb12aaf0d
Mark Nygaard <mark.nygaard@hotmail.com>
2023-10-13 18:11:41 +0200

    test of useServerInsertedHTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from 'components/Header';
 import queryDatoCMS from 'lib/datocms';
 import { HomePageDocument } from 'lib/graphql';
+import { useServerInsertedHTML } from 'next/navigation';
 
 import '../styles/globals.css';
 
@@ -10,6 +11,25 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const data = await queryDatoCMS(HomePageDocument);
+
+  // Colors
+  const primaryColor = data.page?.primaryColor
+    ? `--color-primary: ${data.page.primaryColor.red}, ${data.page.primaryColor.green}, ${data.page.primaryColor.blue};`
+    : '';
+  const secondaryColor = data.page?.secondaryColor
+    ? `--color-secondary: ${data.page.secondaryColor.red}, ${data.page.secondaryColor.green}, ${data.page.secondaryColor.blue};`
+    : '';
+  const accentColor = data.page?.accentColor
+    ? `--color-accent: ${data.page.accentColor.red}, ${data.page.accentColor.green}, ${data.page.accentColor.blue};`
+    : '';
+
+  useServerInsertedHTML(() => {
+    return (
+      <style>
+          :root {`{${primaryColor} ${secondaryColor}  ${accentColor}}`}
+        </style>
+    );
+  });
 
   return (
     <html lang='en'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,8 @@ export default async function Home() {
 
   if (!data?.page) notFound();
 
-  // Colors
-  const primaryColor = `--color-primary: ${data.page.primaryColor!.red}, ${
-    data.page.primaryColor!.green
-  }, ${data.page.primaryColor!.blue};`;
-  const secondaryColor = `--color-secondary: ${
-    data.page.secondaryColor!.red
-  }, ${data.page.secondaryColor!.green}, ${data.page.secondaryColor!.blue};`;
-  const accentColor = `--color-accent: ${data.page.accentColor!.red}, ${
-    data.page.accentColor!.green
-  }, ${data.page.accentColor!.blue};`;
-
   return (
     <>
-      <style>
-        :root {`{${primaryColor} ${secondaryColor}  ${accentColor}}`}
-      </style>
       <div className='bg-skin-primary'>
         {data.page?.content?.map((section: unknown, i: any) => {
           return (
