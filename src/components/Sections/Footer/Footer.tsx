5f7f1486fdaf9b6e0b5354b33a6b29b1ae36e48a
Mark Nygaard <mark.nygaard@hotmail.com>
2023-10-19 22:36:27 +0200

    fixed map

--- src/components/Sections/Footer/Footer.tsx
+++ src/components/Sections/Footer/Footer.tsx
@@ -46,24 +46,26 @@ export default function Footer({
         </div>
       </div>
       <div className='grid grid-cols-1 grid-rows-3'>
-        <div className='col-start-1 col-end-2 row-start-1 row-end-4 px-4 md:px-10 h-60 md:h-80 xl:h-96 container xl:w-3/4 mx-auto'>
-          <div className='mx-4'>
-            <GoogleMap
-              center={center}
-              zoom={17}
-              mapContainerStyle={{
-                width: '100%',
-                height: '100%',
-                borderRadius: '0.375rem',
-              }}
-              options={{
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false,
-              }}
-            >
-              <Marker position={center} />
-            </GoogleMap>
+        <div className='col-start-1 col-end-2 row-start-1 row-end-4 container mx-auto px-4 md:px-10 xl:px-0'>
+          <div className='xl:w-3/4 mx-auto'>
+            <div className='md:mx-4 h-60 md:h-80 xl:h-96'>
+              <GoogleMap
+                center={center}
+                zoom={17}
+                mapContainerStyle={{
+                  width: '100%',
+                  height: '100%',
+                  borderRadius: '0.375rem',
+                }}
+                options={{
+                  streetViewControl: false,
+                  mapTypeControl: false,
+                  fullscreenControl: false,
+                }}
+              >
+                <Marker position={center} />
+              </GoogleMap>
+            </div>
           </div>
         </div>
         <div className='col-start-1 col-end-2 row-start-3 row-end-4 bg-skin-secondary'></div>
@@ -71,7 +73,7 @@ export default function Footer({
       <div className='bg-skin-secondary md:flex md:px-10 xl:pb-16 md:py-8 lg:py-16 p-4'>
         <div className='container mx-auto'>
           <div className='mx-auto xl:w-3/4'>
-            <div className='mx-4 flex justify-center flex-col md:flex-row'>
+            <div className='md:mx-4 flex justify-center flex-col md:flex-row'>
               <div className='prose flex flex-col justify-center pb-8 py-4 prose-p:text-gray-300 prose-a:text-gray-300 prose-strong:text-skin-accent md:w-1/2'>
                 <StructuredText data={contact as any} />
               </div>
