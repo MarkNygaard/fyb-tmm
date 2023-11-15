bd06d0a775cb0aba7755959582d46cfb87d09ecf
Mark Nygaard <mark.nygaard@hotmail.com>
2023-11-15 17:43:06 +0100

    corrected margin and general cleanup

--- src/components/ui/StructuredText/Blocks/CustomHeading/CustomHeading.tsx
+++ src/components/ui/StructuredText/Blocks/CustomHeading/CustomHeading.tsx
@@ -9,7 +9,7 @@ export default function CustomHeading({
   return (
     <div key={id}>
       <div className='m-0 text-2xl font-medium text-white'>{title}</div>
-      <div className='mb-10 mt-0 block text-xl font-medium text-skin-accent'>
+      <div className='mt-0 block text-xl font-medium text-skin-accent xl:mb-8'>
         {subtitle}
       </div>
     </div>
